9a57b2bfd345661e5f4d783035a40bcee18242f0
brian witte <brianwitte@x86.art>
2022-04-25 13:07:34 -0500

    add email

--- config.el
+++ config.el
@@ -13,8 +13,8 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets. It is optional.
-;;(setq user-full-name "John Doe"
-;;      user-mail-address "john@doe.com")
+(setq user-full-name "brian witte"
+      user-mail-address "brianwitte@x86.art")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom:
 ;;
