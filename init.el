684952e7bbd5ab0057543278cfd0e8825585007e
Brian Witte <13672058+brianwitte@users.noreply.github.com>
2023-06-07 15:06:13 -0500

    update init.el

--- init.el
+++ init.el
@@ -150,7 +150,7 @@
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       org               ; organize your plain life in plain text
+       ;;org               ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
@@ -192,7 +192,9 @@
 
        :custom
        (completion +icons +minibuffer)
-       ;;search
+       general
+       search
+       org
        my-modeline
        lsp
        elisp ; always on ;-p
